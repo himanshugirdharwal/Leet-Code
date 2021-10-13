5daa38b21fd20fb3572e2484560e4d519702bdce
himanshugirdharwal <40881180+himanshugirdharwal@users.noreply.github.com>
2021-10-13 16:29:52 +0530

    Create 64. Minimum Path Sum.cpp

---
+++ Dynamic Programm/64. Minimum Path Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        for(int i=1;i<grid.size();i++)
+            grid[i][0]+=grid[i-1][0];
+        
+        for(int j=1;j<grid[0].size();j++)
+            grid[0][j]+=grid[0][j-1];
+            
+        for(int i=1;i<grid.size();i++)
+        {
+            for(int j=1;j<grid[0].size();j++)
+            {
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[grid.size()-1][grid[0].size()-1];
+        /*
+        1 4 5
+        2 7 6
+        6 8 7
+            
+        1 2 3
+        4 5 6
+            
+        1 3 4
+        5 8 10    
+        */
+    }
+};
